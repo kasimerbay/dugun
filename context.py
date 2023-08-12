b16eda1e0d4f2d29acdf8cf0bd630ddacfece064
Ahmet Kasım Erbay <ahmetkasim2@gmail.com>
2023-08-13 02:31:44 +0300

    created project

---
+++ context.py
@@ -0,0 +1,38 @@
+context = {
+    "bride" : "Hatice Beyza",
+    "broom" : "Ahmet Kasım",
+    "ist" : {
+        "dugun" : {
+            "event_name" : "Düğün",
+            "date" : "28 Ekim 2023",
+            "location" : {
+                "name": "Beykoz Koru Sosyal Tesisleri",
+                "address" : "Beykoz / İstanbul",
+                "link" : "https://goo.gl/maps/Z3zmEbBEFD9TN7DW8"
+            },
+            "phrase": "Sizleri düğünümüzde de görmekten şeref duyarız."
+        }
+    },
+    "ant" : {
+        "kina": {
+                "event_name" : "Kına",
+                "date" : "",
+                "location" : {
+                    "name": "",
+                    "address" : "",
+                    "link" : ""
+            },
+            "phrase": ""
+        },
+        "dugun": {
+                "event_name" : "Düğün",
+            "date" : "",
+            "location" : {
+                "name": "",
+                "address" : "",
+                "link" : ""
+            },
+            "phrase": ""
+            }
+    }
+}
